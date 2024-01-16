a73512fb78eee8d9bda92025f061e3ac530afd1d
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2024-01-16 20:20:00 +0000

    Create s3 bucket with cloudfront distribution

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,51 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this.deploymentBucket();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Cognito User Pool
+  }
+
+  deploymentBucket() {
+    // S3 bucket
+    const bucket = new s3.Bucket(this, "DeploymentBucket", {
+      websiteIndexDocument: "index.html",
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+    });
+
+    // Allow public access
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ["s3:GetObject"],
+      resources: [bucket.bucketArn + "/*"],
+      principals: [new iam.AnyPrincipal()],
+    });
+    bucket.addToResourcePolicy(bucketPolicy);
+
+    // Deploy website to S3 bucket
+    new s3Deployment.BucketDeployment(this, "DeployWebsite", {
+      destinationBucket: bucket,
+      sources: [],
+    });
+
+    // Cloudfront distribution
+    new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+    });
   }
 }
